0ea993895912dc756d21115fe7a9e13cbd2196ee
aldox007Networks <aldox007@gmail.com>
2026-06-18 21:46:45 -0700

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 // │  (Supabase → Connect → App Frameworks, o Settings → API)   │
 // └────────────────────────────────────────────────────────────┘
 
-const SUPABASE_URL = "https://abcd1234.supabase.co";       // ej. https://abcd1234.supabase.co
+const SUPABASE_URL = "https://vhhxdjoipcosrbivebtn.supabase.co";       // ej. https://abcd1234.supabase.co
 const SUPABASE_KEY = "sb_publishable_p8lMEufUk00POTBpYOZgbQ_fRkfCfoc";   // empieza con sb_publishable_...
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
